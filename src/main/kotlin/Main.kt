140dbea8c330e58d590152b9b72112fe0580a50c
Luckybar <qazx78489@gmail.com>
2022-10-05 22:36:42 +0800

    將狀態託管

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,56 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val context = Context()
+    context.setsState(ConcreteStateA(context))
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    context.request(35);
 }
+
+class Context{
+    private var mState: State? = null
+
+    fun setsState(state: State){
+        println("setsState: $state")
+        mState = state;
+    }
+
+    fun request(value: Int){
+        mState?.handle(value);
+    }
+}
+
+abstract class State(val context: Context){
+    protected var mContext: Context? = null
+
+    abstract fun handle(value: Int);
+}
+
+class ConcreteStateA(context: Context) : State(context) {
+    override fun handle(value: Int){
+        if(value > 10){
+            println("A State: $value")
+            context.setsState(ConcreteStateB(context))
+            context.request(value)
+        }
+    }
+}
+
+class ConcreteStateB(context: Context) : State(context){
+    override fun handle(value: Int){
+        println("B State: $value")
+        if(value > 20){
+            context.setsState(ConcreteStateC(context))
+            context.request(value)
+        }
+    }
+}
+
+class ConcreteStateC(context: Context) : State(context){
+    override fun handle(value: Int){
+        println("C State: $value")
+        if(value > 30){
+            context.setsState(ConcreteStateA(context))
+            context.request(value)
+        }
+    }
+}
+
